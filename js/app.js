f0e1a4c3c9e2ec2278714a840c3680c7ad4e00fc
Whanderley Fonseca Picado <whanderley.fonseca@globant.com>
2023-04-14 08:58:53 -0600

    Se agrega la seccion acerca de mi y la seccion de experiencia

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ const mobileMenu = document.querySelector('.mobile-menu');
 mobileMenuButton.addEventListener('click', function() {
   mobileMenu.classList.toggle('hidden');
 });
+
+
+const toggleButton = document.querySelector("#toggleButton");
+  const hiddenText = document.querySelector("#hiddenText");
+  
+  toggleButton.addEventListener("click", function() {
+    if (hiddenText.classList.contains("hidden")) {
+      hiddenText.classList.remove("hidden");
+      toggleButton.innerText = "Ver menos";
+    } else {
+      hiddenText.classList.add("hidden");
+      toggleButton.innerText = "Ver más";
+    }
+  });
